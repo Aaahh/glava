2f6b03acfeccb53fecf66a83544eedc3815c4dca
Aaahh <woohoomoo2u@gmail.com>
2018-08-16 00:21:40 -0400

    Add ShaderToy downloader and formater Does not pull channels

---
+++ getshader.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python3
+
+# Standalone Shadertoy
+# Copyright (C) 2014 Simon Budig <simon@budig.de>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import os, sys, json, urllib.request, urllib.parse
+from os.path import expanduser
+
+shaderinfo = """\
+// Shader downloaded from https://www.shadertoy.com/view/%(id)s
+// written by shadertoy user %(username)s
+//
+// Name: %(name)s
+// Description: %(description)s
+"""
+
+shaderdecls = """
+in vec4 gl_FragCoord;
+
+out vec4 fragment;
+
+#request uniform "screen" iResolution
+uniform vec2 iResolution;
+
+#request uniform "iTime" iTime
+uniform float iTime;
+
+// uniform vec3      iResolution;           // viewport resolution (in pixels)
+// uniform float     iTime;                 // shader playback time (in seconds)
+uniform float     iTimeDelta;            // render time (in seconds)
+uniform float     iChannelTime[4];       // channel playback time (in seconds)
+uniform vec3      iChannelResolution[4]; // channel resolution (in pixels)
+uniform vec4      iMouse;                // mouse pixel coords. xy: current (if MLB down), zw: click
+uniform sampler2D iChannel0;             // input channel. XX = 2D/Cube
+uniform sampler2D iChannel1;             // input channel. XX = 2D/Cube
+uniform vec4      iDate;                 // (year, month, day, time in secs)
+uniform float     iSampleRate;           // sound sample rate (i.e., 44100)
+uniform int       iFrame;                // shader playback frame
+\n"""
+
+# see https://www.shadertoy.com/js/effect.js?v=8
+
+shadermain = """\n
+void main (void)
+{
+  vec4 color = vec4 (0.0, 0.0, 0.0, 1.0);
+  mainImage (color, gl_FragCoord.xy);
+  color.w = 1.0;
+  gl_FragColor = color;
+}
+"""
+
+def get_shader (id):
+   url = 'https://www.shadertoy.com/shadertoy'
+   headers = { 'Referer' : 'https://www.shadertoy.com/' }
+   values  = { 's' : json.dumps ({'shaders' : [id]}) }
+
+   data = urllib.parse.urlencode (values).encode ('utf-8')
+   req  = urllib.request.Request (url, data, headers)
+   response = urllib.request.urlopen (req)
+   shader_json = response.read ().decode ('utf-8')
+
+   j = json.loads (shader_json)
+   f = open ("/tmp/current-shader.json", "w")
+   f.write (json.dumps (j, indent=2))
+   f.close ()
+
+   assert (len (j) == 1)
+
+   for s in j:
+      name = s["info"]["name"]
+      desc = s["info"]["description"]
+      desc = "".join (desc.split("\r"))
+      desc = "\n//    ".join (desc.split("\n"))
+      s["info"]["description"] = desc
+      for p in s["renderpass"]:
+         code = (p["code"])
+         return name, code, s["info"]
+
+
+if __name__ == '__main__':
+   if len (sys.argv) < 2:
+      print ("Usage: %s <id>" % sys.argv[0], file=sys.stderr)
+
+   for id in sys.argv[1:]:
+      attempt = 0
+      id = id.split("/")[-1]
+      name, code, info = get_shader (id)
+      code = "".join (code.split ("\r"))
+      f = None
+      home = expanduser("~")
+      while f == None:
+         try:
+            if attempt == 0:
+               fname = "%s" % name
+            else:
+               fname = "%s-%d" % (name, attempt)
+            if not os.path.exists(home+"/.config/glava/"+fname):
+               os.makedirs(home+"/.config/glava/"+fname)
+            f = open (home+"/.config/glava/"+fname+"/1.frag", "x")
+
+         except FileExistsError:
+            attempt += 1
+            if attempt > 10:
+               print ("clean out your files please...", file=sys.stderr)
+               sys.exit (0)
+
+      f.write (shaderinfo % info)
+      f.write (shaderdecls)
+      f.write (code)
+      f.write (shadermain)
+      f.close ()
+      print ("wrote shader to \"%s\"" % (home+"/.config/glava/"+fname+"/1.frag"), file=sys.stderr)
+
